8c9d68c40ac7e545ca5ef161a0a16597a090ebfa
Ulrich Flynn <ulrichflynn9@gmail.com>
2023-11-29 20:35:15 +0100

    FEAT: Paginates category list and lists categories with their meals

--- src/services/category.service.js
+++ src/services/category.service.js
@@ -1,4 +1,4 @@
-const { Category } = require('../models');
+const { Category, Meal } = require('../models');
 
 exports.create = async(body) => {
     return await Category.create(body);
@@ -18,4 +18,55 @@ exports.update = async(id, data) => {
 
 exports.delete = async(_id) => {
     return await Category.deleteOne({ _id });
+}
+
+exports.getPaginated = async(limit, page, order) => {
+    return Category.find()
+    .limit(parseInt(limit) * 1)
+    .skip( (parseInt(page) - 1) * parseInt(limit) )
+    .sort(order == "asc" ? "name" : "-name");
+}
+
+exports.getCategoriesWithTheirMeals = async(limit, page, order) => {
+    return Category.aggregate([
+        {
+          $lookup: {
+            from: Meal.collection.collectionName,
+            localField: "_id",
+            foreignField: "categoryId",
+            as: "meals"
+          }
+        },
+        {
+          $sort: {
+            ["name"]: order == "asc" ? 1 : -1 // 1 for ascending, -1 for descending
+          }
+        },
+        {
+          $skip: parseInt(limit) * (parseInt(page) - 1)
+        },
+        {
+          $limit: parseInt(limit)
+        },
+        {
+          $project: {
+            _id: 1,
+            name: 1,
+            uri: 1,
+            meals: {
+              $map: {
+                input: "$meals",
+                as: "sb",
+                in: {
+                  // Include only the fields you need from schemaB
+                  _id: "$$sb._id",
+                  name: "$$sb.name",
+                  uri: "$$sb.uri",
+                  price: "$$sb.price",
+                }
+              }
+            }
+          }
+        }
+    ]);
 }

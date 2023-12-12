2a16ee2d6f589e635e38cdc8e88573943396408a
Ulrich Flynn <ulrichflynn9@gmail.com>
2023-12-12 22:07:43 +0100

    WIP: Modifies the service that returns branch meals grouped by category

--- src/services/branch.service.js
+++ src/services/branch.service.js
@@ -50,59 +50,206 @@ exports.getPaginated = async (limit, page, order) => {
 }
 
 exports.getBranchMealsGroupedByCategory = async (id) => {
+  // return Branch.aggregate([
+  //   {
+  //     $lookup: {
+  //       from: Meal.collection.collectionName,
+  //       localField: "availableMeals.mealId",
+  //       foreignField: "_id",
+  //       as: "meals"
+  //     }
+  //   },
+  //   {
+  //     $unwind: "$meals"
+  //   },
+  //   {
+  //     $lookup: {
+  //       from: Category.collection.collectionName,
+  //       localField: "meals.categoryId",
+  //       foreignField: "_id",
+  //       as: "category"
+  //     }
+  //   },
+  //   {
+  //     $unwind: "$category"
+  //   },
+  //   {
+  //     $group: {
+  //       _id: "$category._id",
+  //       meals: { $addToSet: "$meals" },
+  //       name: { $first: "$category.name" },
+  //       uri: { $first: "$category.uri" },
+  //     }
+  //   },
+  //   {
+  //     $project: {
+  //       _id: 1,
+  //       name: 1,
+  //       uri: 1,
+  //       meals: {
+  //         $map: {
+  //           input: "$meals",
+  //           as: "sb",
+  //           in: {
+  //             _id: "$$sb._id",
+  //             name: "$$sb.name",
+  //             uri: "$$sb.uri",
+  //             price: "$$sb.price",
+  //             description: "$$sb.description",
+  //             optionIds: "$$sb.optionIds"
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // ]);
+
+
   return Branch.aggregate([
     {
-      $lookup: {
-        from: Meal.collection.collectionName,
-        localField: "availableMeals.mealId",
-        foreignField: "_id",
-        as: "meals"
-      }
+        $unwind: '$availableMeals' // Deconstruct the array
     },
     {
-      $unwind: "$meals"
-    },
-    {
-      $lookup: {
-        from: Category.collection.collectionName,
-        localField: "meals.categoryId",
-        foreignField: "_id",
-        as: "category"
-      }
-    },
-    {
-      $unwind: "$category"
-    },
-    {
-      $group: {
-        _id: "$category._id",
-        meals: { $addToSet: "$meals" },
-        name: { $first: "$category.name" },
-        uri: { $first: "$category.uri" },
-      }
-    },
-    {
-      $project: {
-        _id: 1,
-        name: 1,
-        uri: 1,
-        meals: {
-          $map: {
-            input: "$meals",
-            as: "sb",
-            in: {
-              _id: "$$sb._id",
-              name: "$$sb.name",
-              uri: "$$sb.uri",
-              price: "$$sb.price",
-              description: "$$sb.description",
-              optionIds: "$$sb.optionIds"
-            }
-          }
+        $lookup: {
+            from: Branch.collection.collectionName,
+            let: { availableMealsId: '$availableMeals._id' },
+            pipeline: [
+                {
+                    $match: {
+                        $expr: { $eq: ['$_id', '$$availableMealsId'] }
+                    }
+                },
+                {
+                  $project: {
+                    availableMeals: {
+                          mealId: 1,
+                          inPromo: 1,
+                          promoPrice: 1,
+                          quantity: 1,
+                          endPromoDate: 1,
+                          // Add other fields you want to include
+                      }
+                  }
+              }
+            ],
+            as: 'availableMeals'
         }
-      }
-    }
+    },
+    // {
+    //   $lookup: {
+    //     from: availabilitySchema,
+    //     localField: "availableMeals",
+    //     foreignField: "_id",
+    //     as: "availableMeals"
+    //   }
+    // },
+    // {
+    //   $unwind: "$availableMeals"
+    // },
+    // {
+    //   $lookup: {
+    //     from: Meal.collection.collectionName,
+    //     localField: "availableMeals.mealId",
+    //     foreignField: "_id",
+    //     as: "meals"
+    //   }
+    // },
+    // {
+    //   $unwind: "$meals"
+    // },
+    // {
+    //   $lookup: {
+    //     from: Category.collection.collectionName,
+    //     localField: "meals.categoryId",
+    //     foreignField: "_id",
+    //     as: "category"
+    //   }
+    // },
+    // {
+    //   $unwind: "$category"
+    // },
+    // {
+    //   $group: {
+    //     _id: "$category._id",
+    //     meals: { $addToSet: "$meals" },
+    //     name: { $first: "$category.name" },
+    //     uri: { $first: "$category.uri" },
+    //   }
+    // },
+    // {
+    //   $project: {
+    //     _id: 1,
+    //     name: 1,
+    //     uri: 1,
+    //     availableMeals: {
+    //       $map: {
+    //         input: "$availableMeals",
+    //         as: "available",
+    //         in: {
+    //           _id: "$$available._id",
+    //           inPromo: "$$available.inPromo",
+    //           promoPrice: "$$available.promoPrice",
+    //           quantity: "$$available.quantity",
+    //           endPromoDate: "$$available.endPromoDate",
+    //           meals: {
+    //             $map: {
+    //               input: "$meals",
+    //               as: "sb",
+    //               in: {
+    //                 _id: "$$sb._id",
+    //                 name: "$$sb.name",
+    //                 uri: "$$sb.uri",
+    //                 price: "$$sb.price",
+    //                 loyalties: "$$sb.loyalties",
+    //                 description: "$$sb.description",
+    //                 optionIds: "$$sb.optionIds"
+    //               }
+    //             }
+    //           }
+    //         }
+    //       }
+    //     },
+    //   }
+    // }
   ]);
+
+
+  /**
+ * from: The target collection.
+ * localField: The local join field.
+ * foreignField: The target join field.
+ * as: The name for the results.
+ * pipeline: Optional pipeline to run on the foreign collection.
+ * let: Optional variables to use in the pipeline field stages.
+ */
+// {
+//   from: "branches",
+//   localField: 'availableMeals._id',
+//   foreignField: 'availabilitySchema._id',
+//   as: 'availableMeals',
+//   let: { availableMealsId: '$availableMeals._id' },
+//   pipeline: [
+//     {
+//         $match: {
+//             $expr: { $eq: ['$_id', '$$availableMealsId'] }
+//         }
+//     },
+//     {
+//       $project: {
+//         availableMeals: {
+//               mealId: 1,
+//               inPromo: 1,
+//               promoPrice: 1,
+//               quantity: 1,
+//               endPromoDate: 1,
+//               // Add other fields you want to include
+//           }
+//       }
+//   }
+// ],
+// }
+
+
 }
 
 exports.getNearByBranches = async (coordinates) => {

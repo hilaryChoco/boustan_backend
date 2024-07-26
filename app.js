934738f96c98a709c73a11365038b7ccc0b36035
hilaryChoco <hilarycoder237@gmail.com>
2024-07-26 09:01:46 -0400

    add ome changes in auth.js and app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const session = require('express-session');
 const api_doc = require('./api-doc/doc');
-const morgan = require ('morgan')
+const morgan = require('morgan');
 const routes = require("./src/routes");
 
 app.use(morgan('dev'));
@@ -12,16 +12,15 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
-    
     next();
 });
 
 app.use(
-  session({
-    secret: 'boustan fast food',
-    resave: false,
-    saveUninitialized: false
-  })
+    session({
+        secret: 'boustan fast food',
+        resave: false,
+        saveUninitialized: false
+    })
 );
 
 app.disable('x-powered-by');
@@ -33,43 +32,31 @@ app.use('/api-docs', api_doc);
 app.use('/api', routes);
 
 app.get('/', (req, res) => {
-  return res.send("API server started successfully!")
+    return res.send("API server started successfully!");
 });
 
 app.get("*", (req, res) => {
-  return res.status(404).json({
-    message: "API endpoint not found"
+    return res.status(404).json({
+        message: "API endpoint not found"
     });
 });
 
-
-
-
 /****************** Errors handler ************************/
 
-app.use((req,res,next)=>{
-
-  const error = new Error('Not found');
-  error.status = 404;
-  next(error);
-
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
 });
 
-
-app.use((error, req, res, next)=>{
-
-  res.status(error.status || 500);
-  res.json({
-
-    error:{
-      message:error.message
-    }
-  });
-  
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+        error: {
+            message: error.message
+        }
+    });
 });
-
 
 /*********************************************************/
-
 
 module.exports = app;
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,22 +1,25 @@
 const jwt = require("jsonwebtoken");
 
+console.log('JWT_SECRET:', process.env.JWT_SECRET); // Check the secret
+
 exports.isAuth = (req, res, next) => {
     try {
-      const token = req.headers.authorization ? req.headers.authorization.split(" ")[1] : "";
-      if (!token) {
-        return res.status(400).json({
-          type: "error",
-            message: "A token is required for authentication"
-        });
-      }
-      const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = decodedToken;
-      return next();
+        const token = req.headers.authorization ? req.headers.authorization.split(" ")[1] : "";
+        if (!token) {
+            return res.status(400).json({
+                type: "error",
+                message: "A token is required for authentication"
+            });
+        }
+        console.log('Token:', token); // Check the token
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decodedToken;
+        return next();
     } catch (error) {
-      res.status(401).json({
-        type: "error",
-        message : 'Invalid Token',
-        error: error.stack
-      });
+        res.status(401).json({
+            type: "error",
+            message: 'Invalid Token',
+            error: error.stack
+        });
     }
 };
